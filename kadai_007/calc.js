a841e1348fc5451044785d41adf17eae101a0c58
shigehisa1216 <s.uehara@ace-techno.com>
2024-01-30 10:57:35 +0900

    ここにコミットメッセージを入力

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,17 @@
-let num = Math.floor(Math.random() * 16);
+let num = 4;
 
 // num 出力
 console.log(num);
 
 // if分
-if (num === 3 || num === 6 || num === 9 || num === 12) {
+if (num%3 === 0) {
+  if (num%5 === 0) {
+    console.log('3と5の倍数です');
+  }
+  else
   console.log('3の倍数です');
-}else if (num === 5 || num === 10) {
+}else if (num%5 === 0) {
   console.log('5の倍数です');
-}else if (num === 15) {
-  console.log('3と5の倍数です');
 }else {
   console.log(num);
 }
