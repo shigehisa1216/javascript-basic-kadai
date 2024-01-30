bfa799903cf9bf52d0ea4518f9c5befa84763bc4
shigehisa1216 <s.uehara@ace-techno.com>
2024-01-30 12:19:35 +0900

    課題007_再々提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,17 +1,16 @@
-let num = 4;
+let num = 15;
 
 // num 出力
 console.log(num);
 
 // if分
-if (num%3 === 0) {
-  if (num%5 === 0) {
-    console.log('3と5の倍数です');
-  }
-  else
-  console.log('3の倍数です');
-}else if (num%5 === 0) {
-  console.log('5の倍数です');
-}else {
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数です");
+} else {
   console.log(num);
 }
+
