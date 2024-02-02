ec074a527fcc20e957372ac3445df8d474c09664
shigehisa1216 <s.uehara@ace-techno.com>
2024-02-02 16:29:59 +0900

    課題020の提出

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const Btn = document.getElementById('btn');
+
+const Text = document.getElementById('text');
+
+Btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+})
